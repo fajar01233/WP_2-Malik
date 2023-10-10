b44db49726a059518614e2df2edd8c18f453e5f9
4dh17 <adhityanurhady2006@gmail.com>
2023-10-10 16:55:31 +0700

    menambahkan file untuk pengecekan

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -1,22 +1,26 @@
 <?php
 class matakuliah extends CI_Controller
 {
-    function __construct(){
-        parent::__construct();
-        $this->load->helper('url');
-    }
-    
     public function index()
     {
-        $this->load->view('view-form-matakuliah');
+        $this->load->view('view_form_matakuliah');
     }
+
     public function cetak()
     {
-        $data = [
-            'kode' => $this->input->post('kode'),
-            'nama' => $this->input->post('nama'),
-            'sks' => $this->input->post('sks')
-        ];
-        $this->load->view('view-data-matakuliah', $data);
+        $this->form_validation->set_rules('kode', 'Kode Matakuliah', 'required|min_length[3]', ['required' => 'Kode Matakuliah Harus Diisi', 'min_lenght' => 'Kode Terlalu Pendek']);
+        $this->form_validation->set_rules('nama', 'Nama Matakuliah', 'required|min_length[3]', ['required' => 'Nama Matakuliah Harus Diisi', 'min_lenght' => 'Nama Terlalu Pendek']);
+
+        if ($this->form_validation->run() != true) {
+            $this->load->view('view_form_matakuliah');
+        } else {
+            $data = [
+                'kode' => $this->input->post('kode'),
+                'nama' => $this->input->post('nama'),
+                'sks' => $this->input->post('sks')
+            ];
+
+            $this->load->view('view_data_matakuliah', $data);
+        }
     }
 }
