9254210b2de566c288193c9a40dac7f48ab6d951
4dh17 <adhityanurhady2006@gmail.com>
2023-10-23 21:02:53 +0700

    menambahkan file pertemuan 5

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -4,13 +4,13 @@ class matakuliah extends CI_Controller
     public function index()
     {
         $this->load->view('view-form-matakuliah');
-        $this->load->library('from_validation');
+        
     }
 
     public function cetak()
     {
-        $this->form_validation->set_rules('kode', 'Kode Matakuliah', 'required|min_length[3]', ['required' => 'Kode Matakuliah Harus Diisi', 'min_length' => 'Kode Terlalu Pendek']);
-        $this->form_validation->set_rules('nama', 'Nama Matakuliah', 'required|min_length[3]', ['required' => 'Nama Matakuliah Harus Diisi', 'min_length' => 'Nama Terlalu Pendek']);
+        $this->form_validation->set_rules('kode', 'Kode Matakuliah', 'required|min_length[3]', ['required' => 'Kode Matakuliah Harus Diisi', 'min_lenght' => 'Kode Terlalu Pendek']);
+        $this->form_validation->set_rules('nama', 'Nama Matakuliah', 'required|min_length[3]', ['required' => 'Nama Matakuliah Harus Diisi', 'min_lenght' => 'Nama Terlalu Pendek']);
 
         if ($this->form_validation->run() != true) {
             $this->load->view('view-form-matakuliah');
